4dcbcfdf10a82e8f366286fb81307af379dbf1c5
tharindu97 <tharindukavishna@gmail.com>
2019-10-31 16:38:01 +0530

    startS2

---
+++ main.dart
@@ -0,0 +1,21 @@
+main(List<String> args) {
+  var country = "Sri lanka";
+
+  String name;
+  name = "tharindu";
+
+  bool isTrue = true;
+  bool isFalse = false;
+
+
+  int number = 34;
+  int numberTwo = 2;
+
+  double pi = 3.14;
+  double gravity = 9.8;
+
+  print(pi + gravity);
+  var result = number + numberTwo;
+  print(result);
+
+}
---
+++ step1.dart
@@ -0,0 +1,14 @@
+main(List<String> args){
+
+  int age = 0;
+    if(age >=25){
+      print('this is a man');
+    }else{
+      print('not a man');
+    }
+
+
+    for(var i=0; i<10; i++){
+      print('heloo $i');
+    }
+}
